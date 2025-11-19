e6272fd5ae7edf7a68cbd2db824d6147eb9b51c6
meznah <s202268000@kfupm.edu.sa>
2025-11-19 11:24:53 +0300

    TODO 2

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -7,6 +7,11 @@ const songSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // db schema
-
+const songSchema = new mongoose.Schema({
+    title:  { type: String, required: true, trim: true },
+    artist: { type: String, required: true, trim: true },
+    year:   { type: Number, min: 1900, max: 2100 }
+}, { timestamps: true });
 
 export const Song = mongoose.model("Song", songSchema);
+export default Song;
